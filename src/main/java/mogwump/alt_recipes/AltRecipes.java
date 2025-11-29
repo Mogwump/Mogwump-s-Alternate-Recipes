eb2e4c12a4bd6ff6f41d586288f597ce4f217296
Mogwump <136357423+Mogwump@users.noreply.github.com>
2025-11-29 19:22:49 +0000

    hotfix???

---
+++ src/main/java/mogwump/alt_recipes/AltRecipes.java
@@ -0,0 +1,9 @@
+package mogwump.alt_recipes;
+
+public final class AltRecipes {
+    public static final String MOD_ID = "alt_recipes";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
