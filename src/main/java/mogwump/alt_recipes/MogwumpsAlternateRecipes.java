bfd5d0a0923e5815df9b3ea1bd73718cfe8ca578
Mogwump <136357423+Mogwump@users.noreply.github.com>
2025-11-29 18:49:34 +0000

    hotfix??

--- src/main/java/mogwump/alt_recipes/MogwumpsAlternateRecipes.java
+++
@@ -1,5 +0,0 @@
-package mogwump.alt_recipes;
-
-// The value here should match an entry in the META-INF/neoforge.mods.toml file
-@Mod(MogwumpsAlternateRecipes.mogs_alt_recipes)
-public class MogwumpsAlternateRecipes {}
